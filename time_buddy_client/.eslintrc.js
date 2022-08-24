6dcc11c162bd9d567638d05b7c974633a1a00917
Euan Mendoza <bebopbamf@effectfree.dev>
2022-08-24 20:22:26 +1000

    initial project structure

---
+++ time_buddy_client/.eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['airbnb', 'airbnb/hooks', 'airbnb-typescript', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.eslint.json',
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/function-component-definition': [
+      2,
+      { namedComponents: 'arrow-function' },
+    ],
+  },
+};
