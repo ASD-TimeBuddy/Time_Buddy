bcb88ade99363695e1401bda04be165108e21d33
Jamessmith9956 <42264376+Jamessmith9956@users.noreply.github.com>
2022-08-31 16:36:15 +1000

    API serialization & landing page

--- time_buddy/settings.py
+++ time_buddy/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'time_buddy_events.apps.TimeBuddyEventsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'time_buddy_events.apps.TimeBuddyEventsConfig',
 ]
 
 MIDDLEWARE = [
--- time_buddy_events/views.py
+++ time_buddy_events/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
-
+#from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Event
+from .serializers import Event_Serializer
 # Create your views here.
+
+
+# for info on decorators see - https://stackoverflow.com/questions/6392739/what-does-the-at-symbol-do-in-python
+# @api_view passes the decorated fuction as an argument in the api_view function
+@api_view(['GET'])
+def api_overview(request): #pass in html request
+    api_urls = {
+        'all_events': '/',
+        'Search by User': '/?user=user_name',
+        'Search by Summary': '/?summary=summary_text',
+        'Add': '/create',
+        'Update': '/update/pk',
+        'Delete': '/event/pk/delete'
+    }
+
+    return Response(api_urls)
