ced4ba2b20440053e63977977e7edd5d8b9573b3
Jamessmith9956 <42264376+Jamessmith9956@users.noreply.github.com>
2022-08-31 15:32:01 +1000

    CRUD model setup

--- time_buddy/settings.py
+++ time_buddy/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'time_buddy_events.apps.TimeBuddyEventsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- time_buddy_events/models.py
+++ time_buddy_events/models.py
@@ -1,40 +1,10 @@
 from django.db import models
+import uuid
 
-# Create your models here.
-class event(models.Model):
-    event_id = models.UUIDField(primary_key=True,default=uuid.uuid4, editable=False)
-
-    #BE AWARE: uuid is not strickly unique, remember to take top row in your queries
-    user_id = models.ForeignKey("user_id", verbose_name=_("User ID"), on_delete=models.CASCADE)
-    tz_id = models.ForeignKey("tz_id", verbose_name=_("Time Zone ID"), on_delete=models.CASCADE)
-    location = models.CharField(_("Location"), max_length=250)
-    summary = models.CharField(_("Summary"), max_length=50)
-    description = models.CharField(_("Description"), max_length=500)
-
-    class Meta:
-        db_table = 'events'
-        managed = True
-        verbose_name = 'Event'
-        verbose_name_plural = 'Events'
-
-    def __str__(self):
-        return self.name
-    
-class event_instance(model.Models):
-    instance_id = models.UUIDField("insance_id",verbose_name=_("instance_id"),primary_key=True,default=uuid.uuid4,editable=false)
-    dt_start = models.DateTimeField(_(""), auto_now=False, auto_now_add=False)
-    dt_end = models.DateTimeField(_(""), auto_now=False, auto_now_add=False)
-
-    class Meta:
-        db_table = 'event_instances'
-        managed = True
-        verbose_name = 'Event Instance'
-        verbose_name_plural = 'Event Instances'
-    
-    
-class time_zone(model.Models):
-    tz_id = models.IntegerField("tz_id",verbose_name=_("Time Zone ID")),primary_key=True,unique=True,default=1)
-    tz_name = models.CharField("tz_name",verbose_name=_("Time Zone Name"), max_length=50)
+# Create your models here. 
+class Time_Zone(models.Model):
+    tz_id = models.IntegerField("tz_id",primary_key=True,unique=True,default=1)
+    tz_name = models.CharField("tz_name", max_length=50)
 
     class Meta:
         db_table = 'time_zones'
@@ -42,16 +12,56 @@ class time_zone(model.Models):
         verbose_name = 'Time_Zone'
         verbose_name_plural = 'Time_Zones'
 
-   
     def __str__(self):
-        return self.name
+        return self.tz_id
     
-class user(model.Models):
-    user_id = models.UUIDField("user_id",verbose_name=_("User ID"),primary_key=True,default=uuid.uuid4,editable=false)
+class User(models.Model):
+    user_id = models.UUIDField("user_id",primary_key=True,default=uuid.uuid4,editable=False)
 
     class Meta:
-        db_table = 'users
+        db_table = 'users'
         managed = True
         verbose_name = 'User'
         verbose_name_plural = 'Users'
-    
+
+class Event(models.Model):
+    #BE AWARE: uuid is not strickly unique, remember to take top row in your queries
+    event_id = models.UUIDField(primary_key=True,default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(
+        User, 
+        #allow user to be missing
+        blank=True,
+        null=True, 
+        on_delete=models.CASCADE
+        )
+    tz = models.ForeignKey(
+        Time_Zone, 
+        default='1', 
+        on_delete=models.CASCADE
+        )
+    location = models.CharField("location",max_length=250)
+    summary = models.CharField("summary", max_length=50)
+    description = models.CharField("description", max_length=500)
+
+    class Meta:
+        ordering = ['event_id']
+        db_table = 'events'
+        managed = True
+        verbose_name = 'Event'
+        verbose_name_plural = 'Events'
+
+    def __str__(self):
+        return self.event_id
+
+class Event_Instance(models.Model):
+    instance_id = models.UUIDField("insance_id",primary_key=True,default=uuid.uuid4,editable=False)
+    event_id = models.ForeignKey(Event, on_delete=models.CASCADE)
+    dt_start = models.DateTimeField("dt_start", auto_now=False, auto_now_add=False)
+    dt_end = models.DateTimeField("dt_end", auto_now=False, auto_now_add=False)
+
+    class Meta:
+        ordering = ['instance_id']
+        db_table = 'event_instances'
+        managed = True
+        verbose_name = 'Event Instance'
+        verbose_name_plural = 'Event Instances'
