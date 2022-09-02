973b91aa955d8e63fe87015a7a3aa4019b81c19d
Time_Buddy <huxinlei8@gmail.com>
2022-09-03 00:27:22 +1000

    local time

--- time_buddy_client/src/pages/time-converter.tsx
+++ time_buddy_client/src/pages/time-converter.tsx
@@ -1,5 +1,25 @@
 import { Text } from '@chakra-ui/react';
-
-const TimeConverter = () => <Text>Coming soon...</Text>;
+import { useDate } from '../components/useDate';
+const TimeConverter = () => {
+    
+    const { date, time, wish } = useDate();
+  
+    return (
+      <div className="greetings-container">
+        <h1>
+          {wish}
+         
+        </h1>
+  
+        <div>
+          <h3>
+            {date}
+            <br />
+            {time}
+          </h3>
+        </div>
+      </div>
+    );
+  };
 
 export default TimeConverter;
