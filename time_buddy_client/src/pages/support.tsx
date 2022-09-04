b72fc1fbedfaafa4a72900f9a3e75e550ea88b68
Euan Mendoza <bebopbamf@effectfree.dev>
2022-09-05 01:25:54 +1000

    FAQ initial structure

--- time_buddy_client/src/pages/support.tsx
+++ time_buddy_client/src/pages/support.tsx
@@ -1,5 +1,16 @@
-import { Text } from '@chakra-ui/react';
+import { Stack, Heading, Text, Divider } from '@chakra-ui/react';
 
-const Support = () => <Text>Coming soon...</Text>;
+const Support = () => (
+  <>
+    <Heading size="md">Time Buddy Support</Heading>
+    <Stack spacing="5">
+      <Text fontSize="lg" fontWeight="medium">
+        Frequently Asked Questions
+      </Text>
+      <Divider />
+      <Text>Coming soon...</Text>
+    </Stack>
+  </>
+);
 
 export default Support;
