6603468117278e172ecb12e41eb54150607bc52f
CC0369 <98926546+CC0369@users.noreply.github.com>
2022-09-04 21:21:16 +1000

    Countdown timer till Event : Part 1

--- time_buddy_client/src/pages/time-converter.tsx
+++ time_buddy_client/src/pages/time-converter.tsx
@@ -1,8 +1,12 @@
 import { Text } from '@chakra-ui/react';
 import axios from "axios";
 import { useEffect, useState } from "react";
+import dayjs from 'dayjs';
 
 const TimeConverter = () => {
+
+    //Location and Weather
+
     const [latitude, setLatitude] = useState(0);
     const [longitude, setLongitude] = useState(0);
     const [weather, setWeather] = useState("");
@@ -34,12 +38,61 @@ const TimeConverter = () => {
         fetchLocation();
     }, [latitude, longitude]);
 
+    //Countdown Timer
+
+    const [eventTime, setEventTime] = useState(false);
+    const [days, setDays] = useState(0);
+    const [hours, setHours] = useState(0);
+    const [minutes, setMinutes] = useState(0);
+    const [seconds, setSeconds] = useState(0);
+
+    useEffect(() => {
+        const target = new Date("11/28/2022 13:48:59");
+
+        const interval = setInterval(() => {
+            const now = new Date();
+            const difference = target.getTime() - now.getTime();
+
+            const d = Math.floor(difference / (1000 * 60 * 60 * 24));
+            setDays(d);
+
+            const h = Math.floor(
+                (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+            );
+            setHours(h);
+
+            const m = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+            setMinutes(m);
+
+            const s = Math.floor((difference % (1000 * 60)) / 1000);
+            setSeconds(s);
+
+            if (d <= 0 && h <= 0 && m <= 0 && s <= 0) {
+                setEventTime(true);
+            }
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }, []);
+
+
     return (
         <div className="location">
+            <b><h1>Location:</h1></b>
             <h1>{cityName}</h1>
             <h2>{temperature}°C</h2>
             <h2>{weather}</h2>
-        </div>
+            <h1> </h1>
+            <b><h2>Time till event: </h2></b>
+            <span>{days}</span>
+            <span> Days </span>
+            <span>{hours}</span>
+            <span> Hours </span>
+            <span>{minutes}</span>
+            <span> Minutes </span>
+            <span>{seconds}</span>
+            <span> Seconds </span>
+        </div>  
     );
 
 }
