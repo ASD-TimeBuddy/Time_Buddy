e2b86f3c6793e3c97be9c81d837694c5cad3825f
Jamessmith9956 <42264376+Jamessmith9956@users.noreply.github.com>
2022-09-05 11:26:15 +1000

    creating Django rest api, setup CRUD for Event model

--- time_buddy_events/serializers.py
+++ time_buddy_events/serializers.py
@@ -5,4 +5,4 @@ from .models import Event
 class Event_Serializer(serializers.ModelSerializer):
     class Meta:
         model = Event
-        fields = ('event_id','user','tz','location','summary','description')
+        fields = ('event_id','location','summary','description')
--- time_buddy_events/urls.py
+++ time_buddy_events/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.api_overview, name='home')
+    path('', views.api_overview, name='home'),
+    path('create/', views.add_events, name='add-events'),
+    path('all/', views.view_events, name='view_events'),
+    path('update/<uuid:pk>/', views.update_events, name='update-events'),
+    path('event/<uuid:pk>/delete',views.delete_events, name='delete-events'),
 ]
