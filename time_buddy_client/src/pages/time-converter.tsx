eaad310021f944056a289717edaede0fae673aa1
tasnim1710 <80333990+tasnim1710@users.noreply.github.com>
2022-09-05 11:17:33 +1000

    Update time-converter.tsx

--- time_buddy_client/src/pages/time-converter.tsx
+++ time_buddy_client/src/pages/time-converter.tsx
@@ -1,21 +1,7 @@
 import { Text } from '@chakra-ui/react';
 import React, {useState} from "react";
 import { useForm } from 'react-hook-form';
-import "./App.css";
 
 const TimeConverter = () => <Text>Please enter time conversion details:</Text>;
 
 export default TimeConverter;
-
-function App() {
-    const [values, setValues] = useState({
-        time: "",
-        date: "",
-    });
-    
-    return (
-        <div className="form-container">
-            
-        </div>
-    )
-}
