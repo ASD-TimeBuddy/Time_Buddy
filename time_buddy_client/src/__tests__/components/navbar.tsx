f5ae718ba81140fb6192d61e25cc1770d5078e00
Euan Mendoza <bebopbamf@effectfree.dev>
2022-09-15 18:23:51 +1000

    fixed lint errors

--- time_buddy_client/src/__tests__/components/navbar.tsx
+++ time_buddy_client/src/__tests__/components/navbar.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 
 import ThemeWrapper from '../../_theme_wrapper';
 import Navbar from '../../components/navbar';
