a91cca5eace2025089df47f01e68651e7fad6bbc
Euan Mendoza <bebopbamf@effectfree.dev>
2022-09-29 00:01:18 +1000

    updated client app

---
+++ time_buddy_client/src/components/support/frequently-asked-questions.tsx
@@ -0,0 +1,61 @@
+import { Stack, Box, Text, Divider, Accordion, AccordionItem, AccordionButton, AccordionPanel, AccordionIcon } from '@chakra-ui/react';
+import useSWR from 'swr';
+
+type FAQType = {
+  id: number;
+  question: string;
+  answer: string;
+};
+
+type QuestionAnswerProps = {
+  qa: FAQType;
+};
+
+const QuestionAnswer = (props: QuestionAnswerProps) => {
+  const { qa } = props;
+  const { question, answer } = qa;
+
+  return (
+      <AccordionItem>
+        <AccordionButton>
+<Text
+        fontSize="md"
+        fontWeight="medium"
+      >{question}</Text>
+      <AccordionIcon />
+        </AccordionButton>
+   <AccordionPanel>
+      <Text fontSize="sm">{answer}</Text>
+    </AccordionPanel> 
+      </AccordionItem>
+  
+  );
+};
+
+const FrequentlyAskedQuestions = () => {
+  const { data } = useSWR('http://localhost:8000/support/question-answer');
+
+  if (!data) {
+    return (
+      <Stack spacing="5" mb="8">
+        <Text fontSize="lg" fontWeight="medium">
+          Frequently Asked Questions
+        </Text>
+        <Divider />
+      </Stack>
+    );
+  }
+
+  const { results } = data;
+
+  return (
+    <Accordion>
+{results.map((dataT: FAQType) => (
+        <QuestionAnswer key={results.id} qa={dataT} />
+      ))}
+    </Accordion>
+      
+  );
+};
+
+export default FrequentlyAskedQuestions;
--- time_buddy_client/src/pages/support.tsx
+++ time_buddy_client/src/pages/support.tsx
@@ -1,87 +1,35 @@
-import { Stack, Box, Heading, Text, Divider } from '@chakra-ui/react';
-import useSWR, { Fetcher } from 'swr';
-import { createClient } from '@supabase/supabase-js';
+import { Stack, Flex, Tabs, TabList, Tab, TabPanels, TabPanel, Heading, Text, Divider } from '@chakra-ui/react';
 
-import { supabaseKey, supabaseUrl } from '../constants/supabase';
-
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-const fetchAll: Fetcher<FaqType[], string> = async (table: string) => {
-  const { data: supportFaq, error } = await supabase.from(table).select('*');
-
-  if (error) {
-    // eslint-disable-next-line @typescript-eslint/no-throw-literal
-    throw error;
-  }
-
-  return supportFaq;
-};
-
-type FaqType = {
-  id: number;
-  created_at: string;
-  question: string;
-  answer: string;
-};
-
-type QuestionAnswerProps = {
-  qa: FaqType;
-};
-
-const QuestionAnswer = (props: QuestionAnswerProps) => {
-  const { qa } = props;
-
-  return (
-    <Box>
-      <Text
-        fontSize="md"
-        fontWeight="medium"
-      >{`Question: ${qa.question}`}</Text>
-      <Text fontSize="sm">{`Answer: ${qa.answer}`}</Text>
-    </Box>
-  );
-};
-
-const FrequentlyAskedQuestions = () => {
-  const { data } = useSWR('support_faq', fetchAll);
-
-  if (!data) {
-    return (
-      <Stack spacing="5" mb="8">
-        <Text fontSize="lg" fontWeight="medium">
-          Frequently Asked Questions
-        </Text>
-        <Divider />
-      </Stack>
-    );
-  }
-
-  return (
-    <Stack spacing="5" mb="8">
-      <Text fontSize="lg" fontWeight="medium">
-        Frequently Asked Questions
-      </Text>
-      <Divider />
-      {data.map((dataT: FaqType) => (
-        <QuestionAnswer key={dataT.id} qa={dataT} />
-      ))}
-    </Stack>
-  );
-};
+import FrequentlyAskedQuestions from '../components/support/frequently-asked-questions';
 
 const Support = () => (
   <>
     <Heading size="md" mb="8" data-testid="support-header">
       Time Buddy Support
     </Heading>
-    <FrequentlyAskedQuestions />
-    <Stack spacing="5">
+    <Tabs variant="soft-rounded" colorScheme="blue">
+      <Flex justify="center">
+<TabList>
+        <Tab mx="4">FAQ</Tab>
+        <Tab mx="4">Contact Us</Tab>
+      </TabList>
+      </Flex>
+      
+      <TabPanels>
+        <TabPanel>
+          <FrequentlyAskedQuestions />
+        </TabPanel>
+        <TabPanel>
+<Stack spacing="5">
       <Text fontSize="lg" fontWeight="medium">
         Have a problem? Contact us
       </Text>
       <Divider />
       <Text>Coming soon...</Text>
     </Stack>
+        </TabPanel>
+      </TabPanels>
+    </Tabs>
   </>
 );
 
