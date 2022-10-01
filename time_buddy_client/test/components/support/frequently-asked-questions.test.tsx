0611e8c4917be4f31ee72bf065297ec0690fd8f4
Euan Mendoza <bebopbamf@effectfree.dev>
2022-10-01 16:17:59 +1000

    fixed linting error

--- time_buddy_client/test/components/support/frequently-asked-questions.test.tsx
+++ time_buddy_client/test/components/support/frequently-asked-questions.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import useSWR from 'swr';
+// import useSWR from 'swr';
 
 import Wrapper from '../../wrapper';
 import FrequentlyAskedQuestions from '../../../src/components/support/frequently-asked-questions';
