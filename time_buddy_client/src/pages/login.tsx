deec5151ae2d018e6b36a15a88d5c71f3d425ed0
tasnim1710 <tasnim1710@gmail.com>
2022-10-07 16:35:58 +1100

    login setup

--- time_buddy_client/src/pages/login.tsx
+++ time_buddy_client/src/pages/login.tsx
@@ -1,14 +1,23 @@
-import {
-    Stack,
-    Flex,
-    Tabs,
-    TabList,
-    Tab,
-    TabPanels,
-    TabPanel,
-    Heading,
-    Text,
-    Divider,
-  } from '@chakra-ui/react';
+import React from 'react';
 
-  
+const LoginPage = () => {
+
+    const [credentials, setCredentials] = useState({
+        username: '',
+        password: ''
+    })
+
+    return (
+        <form>
+            <label htmlFor="username">Username</label>
+            <input placeholder="Username"/>
+            <label htmlFor="password">Password</label>
+            <input placeholder="Password"/>
+            <button disabled="true" type = "submit">Login</button>
+        </form>
+    )
+}
+
+export default LoginPage;
+
+
