ccd5892d59b65dd9cb2cd0d53a711f9b6a1628a2
Christie <98926546+CC0369@users.noreply.github.com>
2022-10-07 14:27:05 +0900

    litning

--- time_buddy_client/test/pages/time-converter.test.tsx
+++ time_buddy_client/test/pages/time-converter.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import { act } from 'react-dom/test-utils';
+//import { act } from 'react-dom/test-utils';
 
 import Wrapper from '../wrapper';
 import TimeConverter from '../../src/pages/time-converter';
