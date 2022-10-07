d0f822445a9a16c110e4f484800e0dd98fdf1194
jamessmith9956 <42264376+Jamessmith9956@users.noreply.github.com>
2022-10-07 17:57:59 +1100

    updated URL paths, tests aren't passing yet

--- time_buddy_events/urls.py
+++ time_buddy_events/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.api_overview, name='home'),
-    path('create/', views.add_events, name='add-events'),
-    path('all/', views.view_events, name='view_events'),
-    path('update/<uuid:pk>/', views.update_events, name='update-events'),
-    path('event/<uuid:pk>/delete',views.delete_events, name='delete-events'),
+    path('events/', views.view_events, name='view_events'),
+    path('events/', views.add_events, name='add-events'),
+    path('events/<uuid:pk>', views.update_events, name='update-events'),
+    path('events/<uuid:pk>',views.delete_events, name='delete-events'),
 ]
