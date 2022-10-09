f25ac8ee6083ddb6c40f90e3e92981d8ba2f74b7
Jamessmith9956 <42264376+Jamessmith9956@users.noreply.github.com>
2022-10-10 10:58:33 +1100

    urls

--- time_buddy_events/urls.py
+++ time_buddy_events/urls.py
@@ -3,15 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.api_overview, name='home'),
-<<<<<<< HEAD
     path('events/', views.add_events, name='add-events'),
     path('events/', views.view_events, name='view_events'),
     path('event/<uuid:pk>/', views.update_events, name='update-events'),
     path('event/<uuid:pk>/',views.delete_events, name='delete-events'),
-=======
-    path('events/', views.view_events, name='view_events'),
-    path('events/', views.add_events, name='add-events'),
-    path('events/<uuid:pk>', views.update_events, name='update-events'),
-    path('events/<uuid:pk>',views.delete_events, name='delete-events'),
->>>>>>> d0f822445a9a16c110e4f484800e0dd98fdf1194
 ]
