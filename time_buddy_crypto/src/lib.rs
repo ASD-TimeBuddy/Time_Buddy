198e7d210ffccc9c7a5fcfeacd51d3d06bb90666
Euan Mendoza <bebopbamf@effectfree.dev>
2022-10-10 11:56:35 +1100

    initial crypto libraries

---
+++ time_buddy_crypto/src/lib.rs
@@ -0,0 +1,23 @@
+mod utils;
+
+use ring::digest;
+use wasm_bindgen::prelude::*;
+
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen]
+extern "C" {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet() {
+    alert("Hello, time-buddy-crypto!");
+}
+
+#[wasm_bindgen]
+pub fn hash_email(email: &str) -> String {}
