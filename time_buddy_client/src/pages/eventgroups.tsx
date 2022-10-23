6251ae2d1485449cbdda6ed27af98c8ff6c34faf
Euan Mendoza <bebopbamf@effectfree.dev>
2022-10-24 00:42:22 +1100

    fixed formatting

--- time_buddy_client/src/pages/eventgroups.tsx
+++ time_buddy_client/src/pages/eventgroups.tsx
@@ -1,51 +1,54 @@
-
 import {
+  Box,
+  Button,
+  Container,
+  Stack,
+  FormControl,
+  Input,
+  Text,
+  useColorModeValue,
+} from '@chakra-ui/react';
 
-    Box,
-    Button,
-    Container,
-    Stack,
-    FormControl,
-    Input,
-    Text,
-    useColorModeValue,
-  } from '@chakra-ui/react';
-  
-  
 const EventGroup = () => (
-    <Container maxW="3xl" py="4">
+  <Container maxW="3xl" py="4">
     <Box
-        bg="bg-surface"
-        boxShadow={useColorModeValue('sm', 'sm-dark')}
-        borderRadius="lg"
-        p={{ base: '4', md: '6' }}
+      bg="bg-surface"
+      boxShadow={useColorModeValue('sm', 'sm-dark')}
+      borderRadius="lg"
+      p={{ base: '4', md: '6' }}
     >
-        <Stack spacing="5">
+      <Stack spacing="5">
         <Stack spacing="3">
-            <Text fontSize="lg" fontWeight="medium">
-                Event Groups
-            </Text>
-            <Text fontSize="sm" color="muted">
-                Join an event group.
-            </Text>
-            <FormControl isRequired>
-                <Input type='password' fontSize="sm" color="muted" placeholder='Enter password' value={group_password} />
-            </FormControl>
+          <Text fontSize="lg" fontWeight="medium">
+            Event Groups
+          </Text>
+          <Text fontSize="sm" color="muted">
+            Join an event group.
+          </Text>
+          <FormControl isRequired>
+            <Input
+              type="password"
+              fontSize="sm"
+              color="muted"
+              placeholder="Enter password"
+              value={group_password}
+            />
+          </FormControl>
 
-            <Button bg="bg-accent" color="on-accent" size='sm' width='100px' onClick={joinGroup}>
-                Join
-            </Button>
-            
+          <Button
+            bg="bg-accent"
+            color="on-accent"
+            size="sm"
+            width="100px"
+            onClick={joinGroup}
+          >
+            Join
+          </Button>
         </Stack>
-     
-        </Stack>
+      </Stack>
     </Box>
-    </Container>
+  </Container>
 );
-const joinGroup = () => (
-     
-    console.log('join group')
-  
-);
-  
+const joinGroup = () => console.log('join group');
+
 export default EventGroup;
--- time_buddy_client/src/pages/organisation.tsx
+++ time_buddy_client/src/pages/organisation.tsx
@@ -1,6 +1,5 @@
 import { Link as RRLink } from 'react-router-dom';
 import {
-
   Box,
   Button,
   Container,
@@ -11,39 +10,48 @@ import {
   useColorModeValue,
 } from '@chakra-ui/react';
 
-
 const Organisation = () => (
-    <Container maxW="3xl" py="4">
-      <Box
-        bg="bg-surface"
-        boxShadow={useColorModeValue('sm', 'sm-dark')}
-        borderRadius="lg"
-        p={{ base: '4', md: '6' }}
-      >
-        <Stack spacing="5">
-          <Stack spacing="3">
-            <Text fontSize="lg" fontWeight="medium">
-              Organisation
-            </Text>
-            <Text fontSize="sm" color="muted">
-              Join an organisation.
-            </Text>
-            <FormControl isRequired>
-                <Input type='text' fontSize="sm" color="muted" placeholder='Enter a unique 6 digit ID' value={organisation_id} />
-            </FormControl>
-            <Button bg="bg-accent" color="on-accent" size='sm' width='100px' as={RRLink} to="/eventgroups" onClick={joinOrg}>
-                Join
-            </Button>
-          </Stack>
-   
+  <Container maxW="3xl" py="4">
+    <Box
+      bg="bg-surface"
+      boxShadow={useColorModeValue('sm', 'sm-dark')}
+      borderRadius="lg"
+      p={{ base: '4', md: '6' }}
+    >
+      <Stack spacing="5">
+        <Stack spacing="3">
+          <Text fontSize="lg" fontWeight="medium">
+            Organisation
+          </Text>
+          <Text fontSize="sm" color="muted">
+            Join an organisation.
+          </Text>
+          <FormControl isRequired>
+            <Input
+              type="text"
+              fontSize="sm"
+              color="muted"
+              placeholder="Enter a unique 6 digit ID"
+              value={organisation_id}
+            />
+          </FormControl>
+          <Button
+            bg="bg-accent"
+            color="on-accent"
+            size="sm"
+            width="100px"
+            as={RRLink}
+            to="/eventgroups"
+            onClick={joinOrg}
+          >
+            Join
+          </Button>
         </Stack>
-      </Box>
-    </Container>
- );
+      </Stack>
+    </Box>
+  </Container>
+);
 
- const joinOrg = () =>  (
-    console.log('join organisation')
-
- );
+const joinOrg = () => console.log('join organisation');
 
 export default Organisation;
