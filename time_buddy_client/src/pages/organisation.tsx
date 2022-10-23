34f6669fe70d8ecfec93fa041d62c9b19cfa895e
Euan Mendoza <bebopbamf@effectfree.dev>
2022-10-24 01:11:53 +1100

    fixed formatting

--- time_buddy_client/src/pages/organisation.tsx
+++ time_buddy_client/src/pages/organisation.tsx
@@ -1,6 +1,5 @@
 import { Link as RRLink } from 'react-router-dom';
 import {
-
   Box,
   Button,
   Container,
@@ -11,34 +10,45 @@ import {
   useColorModeValue,
 } from '@chakra-ui/react';
 
-
 const Organisation = () => (
-    <Container maxW="3xl" py="4">
-      <Box
-        bg="bg-surface"
-        boxShadow={useColorModeValue('sm', 'sm-dark')}
-        borderRadius="lg"
-        p={{ base: '4', md: '6' }}
-      >
-        <Stack spacing="5">
-          <Stack spacing="3">
-            <Text fontSize="lg" fontWeight="medium">
-              Organisation
-            </Text>
-            <Text fontSize="sm" color="muted">
-              Join an organisation.
-            </Text>
-            <FormControl isRequired>
-                <Input type='text' fontSize="sm" color="muted" placeholder='Enter a unique 6 digit ID' />
-            </FormControl>
-            <Button bg="bg-accent" color="on-accent" size='sm' width='100px' as={RRLink} to="/eventgroups">
-                Join
-            </Button>
-          </Stack>
-   
+  <Container maxW="3xl" py="4">
+    <Box
+      bg="bg-surface"
+      boxShadow={useColorModeValue('sm', 'sm-dark')}
+      borderRadius="lg"
+      p={{ base: '4', md: '6' }}
+    >
+      <Stack spacing="5">
+        <Stack spacing="3">
+          <Text fontSize="lg" fontWeight="medium">
+            Organisation
+          </Text>
+          <Text fontSize="sm" color="muted">
+            Join an organisation.
+          </Text>
+          <FormControl isRequired>
+            <Input
+              type="text"
+              fontSize="sm"
+              color="muted"
+              placeholder="Enter a unique 6 digit ID"
+            />
+          </FormControl>
+          <Button
+            bg="bg-accent"
+            color="on-accent"
+            size="sm"
+            width="100px"
+            as={RRLink}
+            to="/eventgroups"
+          >
+            Join
+          </Button>
         </Stack>
-      </Box>
-    </Container>
-  );
+      </Stack>
+    </Box>
+  </Container>
+);
 
 export default Organisation;
+
