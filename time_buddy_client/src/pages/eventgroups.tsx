ac069a1cced99807bfd04a60cd40eb09a051dce6
Euan Mendoza <bebopbamf@effectfree.dev>
2022-10-24 01:29:23 +1100

    fixed smol linting warnings

--- time_buddy_client/src/pages/eventgroups.tsx
+++ time_buddy_client/src/pages/eventgroups.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import {
   Box,
   Button,
@@ -9,46 +10,51 @@ import {
   useColorModeValue,
 } from '@chakra-ui/react';
 
-const EventGroup = () => (
-  <Container maxW="3xl" py="4">
-    <Box
-      bg="bg-surface"
-      boxShadow={useColorModeValue('sm', 'sm-dark')}
-      borderRadius="lg"
-      p={{ base: '4', md: '6' }}
-    >
-      <Stack spacing="5">
-        <Stack spacing="3">
-          <Text fontSize="lg" fontWeight="medium">
-            Event Groups
-          </Text>
-          <Text fontSize="sm" color="muted">
-            Join an event group.
-          </Text>
-          <FormControl isRequired>
-            <Input
-              type="password"
-              fontSize="sm"
-              color="muted"
-              placeholder="Enter password"
-              value={group_password}
-            />
-          </FormControl>
-
-          <Button
-            bg="bg-accent"
-            color="on-accent"
-            size="sm"
-            width="100px"
-            onClick={joinGroup}
-          >
-            Join
-          </Button>
-        </Stack>
-      </Stack>
-    </Box>
-  </Container>
-);
 const joinGroup = () => console.log('join group');
+
+const EventGroup = () => {
+  const [groupPassword] = useState('');
+
+  return (
+    <Container maxW="3xl" py="4">
+      <Box
+        bg="bg-surface"
+        boxShadow={useColorModeValue('sm', 'sm-dark')}
+        borderRadius="lg"
+        p={{ base: '4', md: '6' }}
+      >
+        <Stack spacing="5">
+          <Stack spacing="3">
+            <Text fontSize="lg" fontWeight="medium">
+              Event Groups
+            </Text>
+            <Text fontSize="sm" color="muted">
+              Join an event group.
+            </Text>
+            <FormControl isRequired>
+              <Input
+                type="password"
+                fontSize="sm"
+                color="muted"
+                placeholder="Enter password"
+                value={groupPassword}
+              />
+            </FormControl>
+
+            <Button
+              bg="bg-accent"
+              color="on-accent"
+              size="sm"
+              width="100px"
+              onClick={joinGroup}
+            >
+              Join
+            </Button>
+          </Stack>
+        </Stack>
+      </Box>
+    </Container>
+  );
+};
 
 export default EventGroup;
--- time_buddy_client/src/pages/organisation.tsx
+++ time_buddy_client/src/pages/organisation.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Link as RRLink } from 'react-router-dom';
 import {
   Box,
@@ -10,48 +11,52 @@ import {
   useColorModeValue,
 } from '@chakra-ui/react';
 
-const Organisation = () => (
-  <Container maxW="3xl" py="4">
-    <Box
-      bg="bg-surface"
-      boxShadow={useColorModeValue('sm', 'sm-dark')}
-      borderRadius="lg"
-      p={{ base: '4', md: '6' }}
-    >
-      <Stack spacing="5">
-        <Stack spacing="3">
-          <Text fontSize="lg" fontWeight="medium">
-            Organisation
-          </Text>
-          <Text fontSize="sm" color="muted">
-            Join an organisation.
-          </Text>
-          <FormControl isRequired>
-            <Input
-              type="text"
-              fontSize="sm"
-              color="muted"
-              placeholder="Enter a unique 6 digit ID"
-              value={organisation_id}
-            />
-          </FormControl>
-          <Button
-            bg="bg-accent"
-            color="on-accent"
-            size="sm"
-            width="100px"
-            as={RRLink}
-            to="/eventgroups"
-            onClick={joinOrg}
-          >
-            Join
-          </Button>
-        </Stack>
-      </Stack>
-    </Box>
-  </Container>
-);
-
 const joinOrg = () => console.log('join organisation');
+
+const Organisation = () => {
+  const [organisationId] = useState('');
+
+  return (
+    <Container maxW="3xl" py="4">
+      <Box
+        bg="bg-surface"
+        boxShadow={useColorModeValue('sm', 'sm-dark')}
+        borderRadius="lg"
+        p={{ base: '4', md: '6' }}
+      >
+        <Stack spacing="5">
+          <Stack spacing="3">
+            <Text fontSize="lg" fontWeight="medium">
+              Organisation
+            </Text>
+            <Text fontSize="sm" color="muted">
+              Join an organisation.
+            </Text>
+            <FormControl isRequired>
+              <Input
+                type="text"
+                fontSize="sm"
+                color="muted"
+                placeholder="Enter a unique 6 digit ID"
+                value={organisationId}
+              />
+            </FormControl>
+            <Button
+              bg="bg-accent"
+              color="on-accent"
+              size="sm"
+              width="100px"
+              as={RRLink}
+              to="/eventgroups"
+              onClick={joinOrg}
+            >
+              Join
+            </Button>
+          </Stack>
+        </Stack>
+      </Box>
+    </Container>
+  );
+};
 
 export default Organisation;
